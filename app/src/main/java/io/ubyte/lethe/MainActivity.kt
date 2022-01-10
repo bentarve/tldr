084c854a7e2119575865b91b6452e5b2fec22542
Bent Arve <bentarve@gmail.com>
2022-01-10 02:45:42 +0100

    Initial commit

---
+++ app/src/main/java/io/ubyte/lethe/MainActivity.kt
@@ -0,0 +1,38 @@
+package io.ubyte.lethe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import io.ubyte.lethe.ui.theme.LetheTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LetheTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LetheTheme {
+        Greeting("Android")
+    }
+}
