ca97af0a886e7e631927629f8799136dda55fcb2
Bent Arve <bentarve@gmail.com>
2022-08-25 02:45:52 +0200

    remove preview

--- app/src/main/java/io/ubyte/tldr/compose/SectionHeader.kt
+++ app/src/main/java/io/ubyte/tldr/compose/SectionHeader.kt
@@ -3,10 +3,6 @@ package io.ubyte.tldr.compose
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun SectionHeader(text: String) {
@@ -15,11 +11,4 @@ fun SectionHeader(text: String) {
         maxLines = 1,
         style = MaterialTheme.typography.h3
     )
-}
-
-// https://stackoverflow.com/questions/71812710/can-no-longer-view-jetpack-compose-previews-failed-to-instantiate-one-or-more-c
-@Composable
-@Preview
-private fun HeadingTextPreview() {
-    SectionHeader(text = "Section title")
 }
